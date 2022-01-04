bce866549f712bf37e900d6e2544cc7f0abda791
unknown <omkargije@gmail.com>
2022-01-04 14:32:31 +0530

    All Stage

---
+++ src/main/java/SimpliLearn/sportsshoes/App.java
@@ -0,0 +1,13 @@
+package SimpliLearn.sportsshoes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/SimpliLearn/sportsshoes/unitTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package SimpliLearn.sportsshoes;
+
+import junit.framework.TestCase;
+
+/**
+ * @author omkar
+ *
+ */
+public class unitTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
